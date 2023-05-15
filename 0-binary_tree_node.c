c8d125259f234e2e361143d13a87c2e6985c543c
nimrod02 <letrun.a@gmail.com>
2023-05-15 12:16:18 +0200

    add betty syntax to the 0-task and header file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,22 +8,21 @@
  * Return:  a pointer to the new node, or NULL on failure
  */
 
- binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
- {
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
 	if (newNode == NULL)
 	{
-		free (newNode);
+		free(newNode);
 		return (NULL);
 	}
 
-    newNode->n = value;
-    newNode->parent = parent;
-    newNode->left = NULL;
-    newNode->right = NULL;
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
-    return newNode;
-    free (newNode);
-
- }
+	return (newNode);
+	free(newNode);
+}
