788aa3aff63e9612256482f2687797d3b989991a
Spark4545 <lerouxpaulhenri@gmail.com>
2023-05-16 09:50:19 +0000

    Add task 6, 7, 8 orders and task 14 balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,48 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measure the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree\
+ *
+ * Return: the balance factor
+*/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int nodeQuantity = 1;
+
+	if (!tree)
+		return (0);
+
+	return (balance(nodeQuantity, tree->left) - balance(nodeQuantity, tree->right));
+}
+
+/**
+ * balance - calculates the balance factor of a binary tree
+ * @nodeQuantity: quantity of node
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: the balance factor
+*/
+
+int balance(int nodeQuantity, const binary_tree_t *tree)
+{
+	int left = 0, right = 0, result = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left = balance(nodeQuantity + 1, tree->left);
+
+	if (tree->right)
+		right = balance(nodeQuantity + 1, tree->right);
+
+	if (left > right)
+		result = left + 1;
+
+	else
+		result = right + 1;
+
+	return (result);
+}
---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function that goes through a binary tree
+ * using a inorder traversal
+ * @tree: pointer to the root node of the tree
+ * @func: pointer to function to call for each node
+*/
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	if (tree->left)
+		binary_tree_inorder(tree->left, func);
+
+	func(tree->n);
+
+	if (tree->right)
+		binary_tree_inorder(tree->right, func);
+}
